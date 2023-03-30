fc697ddc35f5e40f91f3b200029dd0451da7d571
Joseph-Mukuthi <joseph.mukuthi@student.moringaschool.com>
2023-03-30 11:24:45 +0300

    Use while loops to send messages

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(arr, event){
+    const messages = [];
+    for (let i = 0; i < arr.length; i++){
+        let message = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+console.log(writeCards(names, "surprise"));
+
+function countDown(num){
+    let i = 0;
+    while (i <= num){
+        console.log(num);
+        num--;
+    }
+    return i;
+}
+console.log(countDown(10));
