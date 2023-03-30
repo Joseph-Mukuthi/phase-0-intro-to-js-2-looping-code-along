b23dde076c3fff27dc023e21e90abdb4a089393e
Joseph-Mukuthi <joseph.mukuthi@student.moringaschool.com>
2023-03-30 11:29:14 +0300

    Add comments to code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const names = ["Guadalupe", "Ollie", "Aki"];
+const names = ["Guadalupe", "Ollie", "Aki"]; // Names to loop through
 
 function writeCards(arr, event){
+    /**Used to create thank you messages
+     * from a list of names then appends to messages array
+     */
     const messages = [];
     for (let i = 0; i < arr.length; i++){
         let message = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
@@ -11,6 +14,9 @@ function writeCards(arr, event){
 console.log(writeCards(names, "surprise"));
 
 function countDown(num){
+    /**Count down from given number
+     * all the way down to zero
+     */
     let i = 0;
     while (i <= num){
         console.log(num);
